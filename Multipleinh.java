9a8b758a68535650895591d6462ee944ead371af
sachualex1997 <sachulx@gmail.com>
2019-03-12 14:25:21 +0530

    Multipleinheritance

---
+++ Multipleinh.java
@@ -0,0 +1,33 @@
+class A
+{
+    public void Car()
+     {
+      System.out.println("Audi car");  
+     }
+    
+}
+class B extends A
+    {
+        public void Bike()
+        {
+            System.out.println("Duke bike");
+        }
+    }
+    class C extends B
+    {
+        public void Cycle()
+        {
+            System.out.println("HERO CYCLE");
+        }
+    }
+    
+public class Multipleinh
+{
+    public static void main(String[] args) 
+    {
+        C ob=new C();
+        ob.Car();
+        ob.Bike();
+        ob.Cycle();
+    }
+}
